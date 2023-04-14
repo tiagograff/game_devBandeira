fb92337a83760bdb964151f9d8179b57c51cdc81
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-04-14 10:00:41 -0300

    gravidade, pulo e pulo duplo

--- scenes/player/mask_dude_texture.gd
+++ scenes/player/mask_dude_texture.gd
@@ -1,9 +1,17 @@
 extends Sprite2D
 
+var on_action: bool = false
+
 @export var animation: AnimationPlayer = null
 
 func animate(velocity: Vector2) -> void:
 	change_orientation_based_on_direction(velocity.x)
+	
+	if on_action == true:
+		return
+	if velocity.y != 0:
+		vertical_move_behavior(velocity.y)
+		return
 	horizontal_move_behavior(velocity.x)
 
 func change_orientation_based_on_direction(direction: float) ->void:
@@ -12,8 +20,22 @@ func change_orientation_based_on_direction(direction: float) ->void:
 	if direction < 0:
 		flip_h = true
 
+func action_behavior(action: String) -> void:
+	animation.play(action)
+	on_action = true
+
+func vertical_move_behavior(direction:float) -> void:
+	if direction > 0:
+		animation.play("fall")
+	if direction < 0:
+		animation.play("jump")
+
 func horizontal_move_behavior(direction: float) -> void:
 	if direction != 0:
 		animation.play("run")
 		return
 	animation.play("idle")
+
+
+func on_animation_finished(_anim_name: String):
+	on_action = false
--- scripts/player/mask_dude.gd
+++ scripts/player/mask_dude.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
+var jump_count: int = 0
+var is_on_double_jump : bool = false
+
 @onready var sprite: Sprite2D = get_node("Texture")
 @export var move_speed: float = 96.0
+@export var jump_speed: float = -192.0
+@export var gravity_speed: float = 512.0
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	move()
+	velocity.y += delta * gravity_speed
 	var _move := move_and_slide()
+	jump()
 	sprite.animate(velocity)
 	
 func move() -> void:
@@ -14,5 +21,17 @@ func move() -> void:
 	
 func get_direction() -> float:
 	return(
-		Input.get_axis("ui_left","ui_right")
+		Input.get_axis("walk_left","walk_right")
 	)
+func jump() -> void:
+	if is_on_floor():
+		jump_count = 0
+		is_on_double_jump = false
+		
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		velocity.y = jump_speed
+		jump_count += 1
+		
+	if jump_count == 2 and not is_on_double_jump:
+		sprite.action_behavior("double_jump")
+		is_on_double_jump = true
