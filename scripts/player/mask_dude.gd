57384b77ae791c9d1221583cdaebfef83dc413b1
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-04-15 15:59:54 -0300

    atualização do estado da armadilha

--- scripts/player/mask_dude.gd
+++ scripts/player/mask_dude.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var sprite: Sprite2D = get_node("Texture")
+@onready var stomp_area_collision: CollisionShape2D = get_node("StompArea/Collision")
 
 var is_dead: bool = false
 var jump_count: int = 0
@@ -13,6 +14,7 @@ var max_health: float = 0.0
 @export var move_speed: float = 96.0
 @export var jump_speed: float = -192.0
 @export var gravity_speed: float = 512.0
+@export var damage: int = 5
 
 func _ready() -> void:
 	max_health = health
@@ -46,8 +48,10 @@ func jump() -> void:
 	if is_on_floor():
 		jump_count = 0
 		is_on_double_jump = false
+		stomp_area_collision.set_deferred("disabled", true)
 		
 	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		stomp_area_collision.set_deferred("disabled", false)
 		velocity.y = jump_speed
 		jump_count += 1
 		
@@ -69,3 +73,14 @@ func update_health(target_position: Vector2, value: int, type: String) -> void:
 		is_dead = true
 		sprite.action_behavior("dead")
 	return
+
+func on_stomp_area_entered(area):
+	pass
+	
+func on_stomp_body_entered(body):
+	if body.is_in_group("hazard") and body.is_invinceble == false:
+		body.update_health(damage)
+		
+		knockback_direction = (global_position - body.global_position).normalized()
+		sprite.action_behavior("hit")
+		on_knockback = true
--- scripts/traps/fire.gd
+++ scripts/traps/fire.gd
@@ -4,19 +4,27 @@ extends StaticBody2D
 @onready var animation: AnimationPlayer = get_node("Animation")
 
 var current_state: String = "off"
+var is_invinceble: bool = false
+var max_health: int = 0
 
 @export var damage: int
+@export var health: int = 15
+
+func _ready() -> void:
+	max_health = health
 
 func on_state_timer_timeout():
 	state_timer.start()
 
 	if current_state == "off":
 		current_state = "on"
+		is_invinceble = true
 		animation.play(current_state)
 		return
 		
 	if current_state == "on":
 		current_state = "off"
+		is_invinceble = false
 		animation.play(current_state)
 		return
 
@@ -24,3 +32,21 @@ func on_state_timer_timeout():
 func on_detection_area_body_entered(body) -> void:
 	if body.is_in_group("mask_dude"):
 		body.update_health(global_position, damage,"decrease") 
+		
+func update_health(value:int) -> void:
+	if is_invinceble == true:
+		return
+	health = clamp(health - value , 0, max_health)
+	
+	if health == 0:
+		state_timer.stop()
+		is_invinceble = true
+		current_state = "off"
+		animation.play(current_state)
+		return
+	
+	animation.play("hit")
+
+func on_animation_finished(anim_name):
+	if anim_name == "hit":
+		animation.play(current_state)
